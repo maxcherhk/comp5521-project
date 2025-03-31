ca52c56c45e96d3839c5bc9fd64a825d300889b4
Koby Wong <kobywongchunkit@gmail.com>
2025-03-31 23:08:15 +0800

    add testing script for frontend development

---
+++ hardhat/scripts/transferTokeForTesting.js
@@ -0,0 +1,35 @@
+const { ethers } = require("hardhat");
+const addresses = require("../frontend/src/utils/deployed-addresses.json");  
+// hardhat/frontend/src/utils/deployed-addresses.json
+// hardhat/scripts/transferTokeForTesting.js
+async function main() {
+  // Connect to the Hardhat network
+  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+
+  // Replace with the address of the recipient account
+  const recipientAddress = "xxxx"; // My address (from MetaMask)
+
+  const NewToken = await hre.ethers.getContractFactory("NewToken");
+  const Alpha = NewToken.attach(addresses.tokenA);
+  const Beta = NewToken.attach(addresses.tokenB);
+  const Gamma = NewToken.attach(addresses.tokenC);
+  const Delta = NewToken.attach(addresses.tokenD);
+
+
+  const amount = ethers.parseEther("500000");
+  await Alpha.transfer(recipientAddress, amount)
+  console.log(`Transferred ${amount} Alpha tokens to ${recipientAddress}`);
+  await Beta.transfer(recipientAddress, amount)
+  console.log(`Transferred ${amount} Beta tokens to ${recipientAddress}`);
+  await Gamma.transfer(recipientAddress, amount)
+  console.log(`Transferred ${amount} Gamma tokens to ${recipientAddress}`);
+  await Delta.transfer(recipientAddress, amount)
+  console.log(`Transferred ${amount} Delta tokens to ${recipientAddress}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
